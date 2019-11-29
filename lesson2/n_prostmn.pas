0db28628f6f3f50eb27d8073d8940b0648339084
Mike Ivanov <misha.ivanov.ig@gmail.com>
2019-11-29 22:25:08 +0300

    Add lesson#2
    Разложение числа на простые множители
    
    Signed-off-by: Mike Ivanov <misha.ivanov.ig@gmail.com>

---
+++ lesson2/n_prostmn.pas
@@ -0,0 +1,20 @@
+﻿var i, n, j, a, x:integer;
+begin
+readln(n);
+a:=n;
+write('1');
+for i:=1 to n do
+  begin
+  for j:=2 to i do
+    begin
+    if(i mod j = 0)then
+      x:=x+1;
+      x:=0;
+    if(x<3)and(a mod i = 0)then
+      begin
+      write('*',i);
+      a:=a div i;
+      end;
+    end;
+  end;
+end.
