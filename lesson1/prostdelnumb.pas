c7b361634e07bbd9bc641be0b68ca73e6b31a0dd
Mike Ivanov <misha.ivanov.ig@gmail.com>
2019-11-29 22:19:01 +0300

    Add lesson#1
    Lesson#1 27.11.19
    Найти все делители натурального числа, у которого есть заданная цифра
    
    Signed-off-by: Mike Ivanov <misha.ivanov.ig@gmail.com>

---
+++ lesson1/prostdelnumb.pas
@@ -0,0 +1,20 @@
+﻿var f, i, n, a,c:integer;
+begin
+readln(n,c);
+f:=0;
+for i:=1 to n do
+  if(n mod i = 0)then
+    begin
+    a:=i;
+    f:=0;
+      while(a<>0)and(f=0)do
+        begin
+        if(a mod 10 = c)then
+          begin
+          f:=1;
+          writeln(i);
+          end;
+        a:=a div 10;
+    end;
+  end;  
+end.
