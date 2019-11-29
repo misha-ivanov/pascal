b3fca16269e712e0aad1e81fb76f660e4dba1669
Mike Ivanov <misha.ivanov.ig@gmail.com>
2019-11-29 22:26:17 +0300

    Add lesson#3
    Сравнение числа на четность и последующий вывод  некоторого числа
    
    Signed-off-by: Mike Ivanov <misha.ivanov.ig@gmail.com>

---
+++ lesson3/chetornechet.pas
@@ -0,0 +1,22 @@
+﻿var n, k, a, bm, am:integer;
+begin
+readln(n);
+a:=1;
+am:=0;
+bm:=0;
+while(n>0)do
+begin
+k:=n mod 10;
+if(k mod 2=0)then
+  begin
+  am:=am + k * a;
+  a:=a*10;
+  end
+  else
+  begin
+  bm:=bm*10 + k;
+  end;
+n:=n div 10;
+end;
+writeln(am,bm);
+end.
