1b7eb0160bdb3f348b6ed2e0d9b4f5134ae8159a
Mike Ivanov <misha.ivanov.ig@gmail.com>
2019-12-04 14:32:56 +0300

    lesson#4 04.12.2019 Дано 2 натуральных числа. Найти все делители первого, не являющимися делителями второго, а также среднее арифметическое их общих делителей.
    Signed-off-by: Mike Ivanov <misha.ivanov.ig@gmail.com>

---
+++ lesson4/all_del_1no2__srarifm.pas
@@ -0,0 +1,20 @@
+﻿var a, b, i:integer;
+var k, sa, q:real;
+begin
+readln(a, b);
+sa:=0;
+k:=0;
+q:=0;
+for i:=1 to a do
+begin
+  if(a mod i = 0)and(b mod i <>0)then
+    writeln(i);
+  if(a mod i = 0)and(b mod i = 0)then
+    begin
+    k:=k+i;
+    q:=q+1;
+    end;    
+end;
+  sa:=k/q;
+  writeln('Среднее арифметическое всех делителей введеных чисел равно ',sa);
+end.
