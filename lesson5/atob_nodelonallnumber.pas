c9f84a337a37659ed1412707c5ab1c4e0498dfae
Mike Ivanov <misha.ivanov.ig@gmail.com>
2020-01-04 21:43:22 +0300

    Add lesson#5
    Нахождение чисел на отрезке ab, не делящихся ни на одну из своих цифр
    
    Signed-off-by: Mike Ivanov <misha.ivanov.ig@gmail.com>

---
+++ lesson5/atob_nodelonallnumber.pas
@@ -0,0 +1,19 @@
+﻿var f, a, b, i, n, j:integer;
+
+begin
+readln(a, b);
+for i:=a to b do 
+begin
+j:=i;
+f:=0;
+while j>0 do
+  begin
+    n:=j mod 10;
+    if (n<>0)and(i mod n=0)then
+      f:=1;
+    j:=j div 10;  
+  end;
+if(f=0)then
+  writeln(i);  
+end;
+end.
