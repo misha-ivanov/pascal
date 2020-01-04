48db965888b36a99bd412439b57763eb3c328dd9
Mike Ivanov <misha.ivanov.ig@gmail.com>
2020-01-04 21:51:58 +0300

    Add lesson#7
    Определение, убывающее или возрастающее число.
    
    Signed-off-by: Mike Ivanov <misha.ivanov.ig@gmail.com>

---
+++ lesson7/vozrastnumb.pas
@@ -0,0 +1,19 @@
+﻿var n, a, b, f:integer;
+begin
+readln(n);
+f:=0;
+a:=n mod 10;
+n:=n div 10;
+while n>0 do
+begin
+b:=n mod 10;
+if(a<b)then
+  f:=1;
+a:=b;
+n:=n div 10;
+end;
+if(f=0)then
+  writeln('Возрастающее')
+  else
+  writeln('Убывающее');
+end.
